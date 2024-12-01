b72257790b78d0675f401ab2934fd5f15c5883c6
muhilman2012 <muhi7714@gmail.com>
2024-12-01 13:13:16 +0700

    new

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Muhammad Hilman">
+    <meta name="msapplication-navbutton-color" content="#ffffff" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff" />
+    <link rel="icon" type="image/svg" href="{{asset('/images/logo/logo-setwapres.svg')}}" />
+    <title>Mudik Nataru 2024 bareng Mas Wapres</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Pacifico&display=swap" rel="stylesheet">
+</head>
+  <body>
+      <iframe
+        id="JotFormIFrame-243330600175445"
+        title="DEVELOPMENT FORM PENDAFTARAN MUDIK NATARU 2024"
+        onload="window.parent.scrollTo(0,0)"
+        allowtransparency="true"
+        allow="geolocation; fullscreen"
+        src="https://form.jotform.com/243330600175445"
+        frameborder="0"
+        style="min-width:100%;max-width:100%;height:539px;border:none;"
+        scrolling="no"
+      >
+      </iframe>
+      <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
+      <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-243330600175445']", "https://form.jotform.com/")</script>
+  </body>
+</html>
